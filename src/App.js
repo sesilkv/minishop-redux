08f78cceb39d59170bcc1866de80a9f038b375fe
KIRANA\kirana <kirana0701@gmail.com>
2022-04-22 15:55:02 +0700

    setup-router

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/products" element={<h1>Products</h1>} />
+        <Route path="/categories" element={<h1>Categories</h1>} />
+        <Route path="/shopping-cart" element={<h1>Shopping Cart</h1>} />
+        <Route path="/order-history" element={<h1>Order History</h1>} />
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
